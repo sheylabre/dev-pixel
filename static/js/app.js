8059f906385efc3c78e3220288c06781d45c3e9e
sheylabre <sheylabrena@hotmail.com>
2017-05-26 18:42:50 -0500

    cuadro

--- static/js/app.js
+++ static/js/app.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
 			if(flag){
 				$("#logo").css({"margin-top": "150px", "width": "360px","height":"210px"});
 
-				$("nav").css({"background-color": "#384a8a"});
+				$("nav").css({"background-color": "#52575D"});
 				flag = false;
 			}
 		}
@@ -28,3 +28,25 @@ $(document).ready(function(){
 	});
 
 });
+
+
+
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+     $('.modal').modal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      inDuration: 300, // Transition in duration
+      outDuration: 200, // Transition out duration
+      startingTop: '4%', // Starting top style attribute
+      endingTop: '10%', // Ending top style attribute
+      ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+        alert("Ready");
+        console.log(modal, trigger);
+      },
+      complete: function() { alert('Closed'); } // Callback for Modal close
+    }
+  );
+
+  });
+
